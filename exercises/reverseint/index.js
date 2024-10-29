358768a5fd12a8710670bc97f2834064213cc9f7
Het Mangukiya <hetpmangukiya@gmail.com>
2024-10-29 18:46:15 +0530

    Add reverse int function

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,24 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  const positiveNumberString = String(Math.abs(n));
+  const reversedNumber =
+    Number(positiveNumberString.split('').reverse().join('')) * Math.sign(n);
+
+  return reversedNumber;
+}
+// function reverseInt(n) {
+//   if (n === 0) {
+//     return 0;
+//   }
+//   const isNumberPositive = n > 0;
+//   const positiveNumberString = String(n * (isNumberPositive ? 1 : -1));
+//   const reversedNumber =
+//     Number(positiveNumberString.split('').reverse().join('')) *
+//     (isNumberPositive ? 1 : -1);
+
+//   return reversedNumber;
+// }
 
 module.exports = reverseInt;
