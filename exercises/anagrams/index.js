3ad0d0ee68d167ad94164d7c817dfc2b0ab34514
Het Mangukiya <hetpmangukiya@gmail.com>
2024-10-31 15:33:13 +0530

    Add anagrams function

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,52 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+function anagrams(stringA, stringB) {
+  const strACharMap = createCharMap(stringA);
+  const strBCharMap = createCharMap(stringB);
+
+  if (Object.keys(strACharMap).length !== Object.keys(strBCharMap).length) {
+    return false;
+  }
+
+  const differenceInStrB = [];
+  Object.keys(strACharMap).forEach((char) => {
+    if (!strBCharMap[char]) {
+      differenceInStrB.push(char);
+    }
+  });
+
+  const differenceInStrA = [];
+  Object.keys(strBCharMap).forEach((char) => {
+    if (!strACharMap[char]) {
+      differenceInStrA.push(char);
+    }
+  });
+
+  if (differenceInStrA.length || differenceInStrB.length) {
+    return false;
+  }
+
+  return true;
+}
+
+const createCharMap = (str) => {
+  const charMap = {};
+
+  for (const char of str) {
+    if (!alphabet.includes(char)) {
+      continue;
+    }
+    const lowerChar = char.toLowerCase();
+    if (charMap[lowerChar]) {
+      charMap[lowerChar]++;
+    } else {
+      charMap[lowerChar] = 1;
+    }
+  }
+
+  return charMap;
+};
 
 module.exports = anagrams;
