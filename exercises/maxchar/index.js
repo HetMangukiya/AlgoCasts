1c8b3cf6663812fc151b50bb8292c306b44e3857
Het Mangukiya <hetpmangukiya@gmail.com>
2026-08-19 21:05:18 +0530

    Complete maxchar exercise

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const charMap = {};
+
+  for (const char of str) {
+    if (charMap[char]) {
+      charMap[char]++;
+    } else {
+      charMap[char] = 1;
+    }
+  }
+
+  const maxChar = { char: '', num: 0 };
+
+  for (const [char, num] of Object.entries(charMap)) {
+    if (num > maxChar.num) {
+      maxChar.char = char;
+      maxChar.num = num;
+    }
+  }
+
+  return maxChar.char;
+}
 
 module.exports = maxChar;
