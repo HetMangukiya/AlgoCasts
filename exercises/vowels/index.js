e338a1ff527d98d069952a2ee34c4f5b09371f36
Het Mangukiya <hetpmangukiya@gmail.com>
2026-08-20 17:19:01 +0530

    Complete vowels exercise

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,20 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+// function vowels(str) {
+//   return str.toLowerCase().replace(/[^aeiou]/g, '').length;
+// }
+
+function vowels(str) {
+  let count = 0;
+
+  for (let char of str.toLowerCase()) {
+    if (['a', 'e', 'i', 'o', 'u'].includes(char)) {
+      count++;
+    }
+  }
+
+  return count;
+}
 
 module.exports = vowels;
