929deff923d632791c62fdcbff7604a85b3b7e11
Het Mangukiya <hetpmangukiya@gmail.com>
2026-08-21 21:40:50 +0530

    Complete qfroms exercise

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,49 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.stack1 = new Stack();
+    this.stack2 = new Stack();
+  }
+
+  add(el) {
+    this.stack1.push(el);
+  }
+
+  remove() {
+    // Invert the stack into stack2
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop());
+    }
+
+    // Get the first element which as the last of stack1
+    const lastElement = this.stack2.pop();
+
+    // Invert the stack again into stack1
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop());
+    }
+
+    return lastElement;
+  }
+
+  peek() {
+    // Invert the stack into stack2
+    while (this.stack1.peek()) {
+      this.stack2.push(this.stack1.pop());
+    }
+
+    // Get the first element which as the last of stack1
+    const lastElement = this.stack2.peek();
+
+    // Invert the stack again into stack1
+    while (this.stack2.peek()) {
+      this.stack1.push(this.stack2.pop());
+    }
+
+    return lastElement;
+  }
+}
 
 module.exports = Queue;
