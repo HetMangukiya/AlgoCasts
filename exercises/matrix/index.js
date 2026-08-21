fe2569fe74cd569a366e849f5829235f4274f651
Het Mangukiya <hetpmangukiya@gmail.com>
2026-08-21 14:29:49 +0530

    Complete matrix exercise

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,81 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function createEmptyMatrix(n) {
+  return Array.from({ length: n }, (row) =>
+    Array.from({ length: n }, () => null),
+  );
+}
+
+function getValueAt(matrix, row, column) {
+  return matrix?.[row]?.[column];
+}
+
+function getNextDirection(currentDirection, matrix, row, column, n) {
+  let newDirection = currentDirection;
+  if (
+    currentDirection == 'right' &&
+    (getValueAt(matrix, row, column + 1) !== null || column === n - 1)
+  ) {
+    newDirection = 'down';
+  } else if (
+    currentDirection == 'down' &&
+    (getValueAt(matrix, row + 1, column) || row === n - 1)
+  ) {
+    newDirection = 'left';
+  } else if (
+    currentDirection == 'left' &&
+    (getValueAt(matrix, row, column - 1) || column === 0)
+  ) {
+    newDirection = 'up';
+  } else if (
+    currentDirection == 'up' &&
+    (getValueAt(matrix, row - 1, column) || row === 0)
+  ) {
+    newDirection = 'right';
+  }
+
+  return newDirection;
+}
+
+function matrix(n) {
+  const matrix = createEmptyMatrix(n);
+  // up down left right
+  let direction = 'right';
+
+  let currentIdx = [0, 0];
+  let row = 0;
+  let column = 0;
+  for (let i = 1; i <= n * n; i++) {
+    matrix[row][column] = i;
+
+    direction = getNextDirection(direction, matrix, row, column, n);
+
+    if (direction == 'right') {
+      column++;
+    }
+    if (direction == 'down') {
+      row++;
+    }
+    if (direction == 'left') {
+      column--;
+    }
+    if (direction == 'up') {
+      row--;
+    }
+  }
+
+  return matrix;
+}
+
+// for (let line of matrix(10)) {
+//   let string = '';
+
+//   for (const element of line) {
+//     string += `${element.toString().padEnd((10 * 10).toString().length, ' ')} `;
+//   }
+
+//   console.log(string);
+// }
 
 module.exports = matrix;
