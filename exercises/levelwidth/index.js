5235bc418e5e10e8fb1ed9032a102042dcee1d61
Het Mangukiya <hetpmangukiya@gmail.com>
2026-08-25 20:39:45 +0530

    Complete levelwidth exercise

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,20 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let levels = [1];
+
+  let currentLevel = 1;
+  let levelData = root.children;
+
+  while (levelData.length > 0) {
+    levels[currentLevel] = levelData.length;
+
+    currentLevel++;
+    levelData = levelData.map((child) => child.children).flat();
+  }
+
+  return levels;
+}
 
 module.exports = levelWidth;
