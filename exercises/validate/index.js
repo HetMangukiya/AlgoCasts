d31207122326e11efc88b94d64f34bf7e850578f
Het Mangukiya <hetpmangukiya@gmail.com>
2026-08-26 16:19:43 +0530

    Complete validate exercise

--- exercises/validate/index.js
+++ exercises/validate/index.js
@@ -4,7 +4,28 @@
 // less than the parent node's value, and that
 // every node's right hand child is greater than
 // the parent
+//   10
+//  5  20
+// 0     15
 
-function validate(node, min = null, max = null) {}
+// 10 5 20 0 15
+// 0 5 X10X 15 20
+//   10
+
+function validate(node, min = -Infinity, max = Infinity) {
+  const isNodeValid = node.data < max && node.data > min;
+
+  let isLeftNodeValid = true;
+  if (node.left) {
+    isLeftNodeValid = validate(node.left, min, node.data);
+  }
+
+  let isRightNodeValid = true;
+  if (node.right) {
+    isRightNodeValid = validate(node.right, node.data, max);
+  }
+
+  return isNodeValid && isLeftNodeValid && isRightNodeValid;
+}
 
 module.exports = validate;
